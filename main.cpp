af3bdeb8e66815d3c6cd984d2cc45019564d9859
Simonas Valkiunas <simonas.valkiunas@gmail.com>
2018-04-10 14:45:46 +0300

    v0.2 xd

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <cmath>
 #include <string>
 #include <algorithm>
 #include <iomanip>
+#include <sstream>
 
 struct studentas{
 std::string vardas;
@@ -18,7 +20,61 @@ std::vector<studentas> s;
 int ndpazymis =0;
 
 
+void nuskaityti()
+{
+    std::fstream fd ("kursiokai.txt");
+
+    std::string line;
+    std::string ivertinimai;
+    int pazymys;
+    getline(fd,line);
+    if (fd)
+    {
+        while(getline(fd,line))
+        {
+            studentas *stud = new studentas;
+            (*stud).pazymiai.clear();
+            std::istringstream ss(line);
+            ss>>(*stud).pavarde>>(*stud).vardas;
+            getline(ss,ivertinimai);
+            std::istringstream sss(ivertinimai);
+            for (int i=0; i<5; i++)
+            {
+                sss>>pazymys;
+                (*stud).pazymiai.push_back(pazymys);
+            }
+            sort((*stud).pazymiai.begin(),(*stud).pazymiai.end());
+            sss>>(*stud).egzoPazymys;
+              float ndVidurkis;
+    ndVidurkis = (float)std::accumulate((*stud).pazymiai.begin(),(*stud).pazymiai.end(),0)/(float)(*stud).pazymiai.size();
+    float ndMediana;
+    std::sort((*stud).pazymiai.begin(),(*stud).pazymiai.end());
+    int pozicija = (*stud).pazymiai.size();
+    if (pozicija%2==0)
+    {
+        ndMediana = (float)((*stud).pazymiai[(pozicija/2)-1]+(*stud).pazymiai[pozicija/2])/2;
+    }
+    else ndMediana = (*stud).pazymiai[pozicija/2];
+
+
+            (*stud).galutinisVidurkis = (float)(ndVidurkis*0.4)+(float)(*stud).egzoPazymys*0.6;
+            (*stud).galutinisMediana = (float)(ndMediana*0.4)+(float)(*stud).egzoPazymys*0.6;
+            s.push_back(*stud);
+        }
+
+
+
+    }
+}
+
 void pildyti (){
+    std::cout<<"Ar norite nuskaityti duomenis iš failo? (Taip/Ne)"<<std::endl;
+    std::string TaipNe;
+    std::cin>> TaipNe;
+    if (TaipNe == "Taip")
+        {
+            nuskaityti();
+        }else{
     studentas *stud = new studentas;
     std::cout<<"Studento vardas"<<std::endl;
     std::cin>>(*stud).vardas;
@@ -64,7 +120,7 @@ void pildyti (){
     (*stud).galutinisVidurkis = (float)(ndVidurkis*0.4)+(float)(*stud).egzoPazymys*0.6;
     (*stud).galutinisMediana = (float)(ndMediana*0.4)+(float)(*stud).egzoPazymys*0.6;
     s.push_back(*stud);
-
+    }
 }
 void spausdinti()
 {
@@ -77,6 +133,37 @@ void spausdinti()
         std::cout<<std::setprecision(3)<<s[i].galutinisVidurkis<<std::endl;
     }
 }
+
+
+
+
+void rikiuotiStudentus()
+{
+
+    int i=0;
+
+    int n = s.size();
+    for(i; i<n; i++)
+    {
+        int j=i+1;
+        for (j; j<n; j++)
+        {
+            if(s[i].pavarde > s[j].pavarde)
+            {
+                auto temp = s[i];
+                s[i]= s[j];
+                s[j]= temp;
+            }
+
+        }
+
+    }
+
+
+}
+
+
+
 
 
 
